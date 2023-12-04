7a6c84f194737d13f290d6cd72442e86c1af73d8
Vinícius Cortêz <cortez_vini@hotmail.com>
2023-12-04 02:34:59 -0300

    Add project files.

---
+++ Validators/ContainsStringValidation.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCustomValidators
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class ContainsStringValidation: ValidationAttribute
+    {
+        private string MemberName { get; }
+
+        public ContainsStringValidation(string memberName, string? errorMessage = null)
+        {
+            MemberName = string.IsNullOrEmpty(memberName) ? throw new ArgumentNullException(nameof(memberName)) : memberName;
+            ErrorMessage = errorMessage ?? "Valor contido";
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var valueString = (string?)value;
+
+            if (string.IsNullOrEmpty(valueString))
+                return ValidationResult.Success;
+
+            IEnumerable<string>? memberValue;
+            try
+            {
+                memberValue = (IEnumerable<string>?)validationContext.ObjectType.GetProperty(MemberName)?.GetValue(validationContext.ObjectInstance);
+
+                memberValue ??= (IEnumerable<string>?)validationContext.ObjectType.GetField(MemberName)?.GetValue(validationContext.ObjectInstance);
+
+                if (memberValue == null)
+                {
+                    throw new ArgumentException($"Não foi possível encontrar o membro {MemberName}. Verifique se ele foi definido.");
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                throw new InvalidCastException($"Não foi possível fazer o cast do membro {MemberName}. O membro dever ser do tipo \"IEnumerable<string>\".", ex);
+            }
+
+            return memberValue.Contains(valueString) ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+        }
+    }
+}
